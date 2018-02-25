3d53339217124cab4c94448c908fea901715a579
Cooper Luetje <cooper@bytelion.com>
2018-02-25 12:41:28 -0500

    Added tutor page

--- HomeworkPlus/HomeworkPlus/DescriptionPage.xaml.cs
+++ HomeworkPlus/HomeworkPlus/DescriptionPage.xaml.cs
@@ -29,6 +29,11 @@ namespace HomeworkPlus
             {
                 Navigation.PushAsync(new ViewQuestions());
             };
+
+            TutorButton.Clicked += delegate
+            {
+                Navigation.PushAsync(new TutorPage());
+            };
         }
 	}
 }
---
+++ HomeworkPlus/HomeworkPlus/TutorPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Plugin.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeworkPlus
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TutorPage : ContentPage
+	{
+		public TutorPage ()
+		{
+			InitializeComponent ();
+
+            string subject = CrossSettings.Current.GetValueOrDefault("subject", "");
+            SubjectLabel.Text = subject;
+        }
+	}
+}
